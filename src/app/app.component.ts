7ca1ce7ebc15f87ea1ab1707890379c18b66cde2
iandoyle <get@iandoyle.pro>
2016-10-18 22:55:09 -0400

    new achievement unlocked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App: DEPLOYED ON AZURE</h1>'
+    template: '<h1>My First Angular 2 App: DEPLOYED ON AZURE</h1>'
 })
 export class AppComponent { }
