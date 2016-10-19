d787759b215672cc425fda62b180f689a8637247
iandoyle <get@iandoyle.pro>
2016-10-18 23:03:10 -0400

    Remove duplicated directory

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App: DEPLOYED ON AZURE</h1>'
-})
-export class AppComponent { }
